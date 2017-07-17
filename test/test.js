85bb9475516472085372291f445d0dc8ba3ce5b8
Ralph Smith <ralphsmith80@gmail.com>
2017-07-17 17:07:06 -0600

    make this work publicly

--- test/test.js
+++ test/test.js
@@ -7,74 +7,74 @@ describe('Load a Page', function () {
 
   let nightmare = null
   beforeEach(() => {
-    nightmare = new Nightmare()
-    // nightmare = new Nightmare({show: true})
+    // nightmare = new Nightmare()
+    nightmare = new Nightmare({show: true})
   })
 
-  // describe('/ (Home Page)', () => {
+  describe('/ (Home Page)', () => {
+    it('should load without error', done => {
+      // your actual testing urls will likely be `http://localhost:port/path`
+      nightmare
+        .goto('https://gethoodie.com')
+        .end()
+        .then(result => { done() })
+        .catch(done)
+    })
+  })
+
+  // describe('load operator console login page', () => {
   //   it('should load without error', done => {
   //     // your actual testing urls will likely be `http://localhost:port/path`
   //     nightmare
-  //       .goto('https://gethoodie.com')
+  //       .goto('http://10.238.41.200/operatorconsole_portlet/standalone_login.jsp')
   //       .end()
   //       .then(result => { done() })
   //       .catch(done)
   //   })
   // })
-
-  describe('load operator console login page', () => {
-    it('should load without error', done => {
-      // your actual testing urls will likely be `http://localhost:port/path`
-      nightmare
-        .goto('http://10.238.41.200/operatorconsole_portlet/standalone_login.jsp')
-        .end()
-        .then(result => { done() })
-        .catch(done)
-    })
-  })
-  describe('sign in to login page', () => {
-    it('should work without error', done => {
-      nightmare
-        .goto('http://10.238.41.200/operatorconsole_portlet/standalone_login.jsp')
-        .type('#usrname', 'administrator')
-        .type('#pswd', 'password')
-        .click('.submit')
-        .wait('#root')
-        .end()
-        .then(result => { done() })
-        .catch(done)
-    })
-  })
-  describe('intial page load', () => {
-    it('should have a title bar logo', done => {
-      nightmare
-        .goto('http://10.238.41.200/operatorconsole_portlet/standalone_login.jsp')
-        .type('#usrname', 'administrator')
-        .type('#pswd', 'password')
-        .click('.submit')
-        .wait('.titlebarheader__logo')
-        .exists('.titlebarheader__logo')
-        .end()
-        .then(result => {
-          assert.ok(result)
-          done()
-        })
-        .catch(done)
-    })
-    it('should have a primary nav', done => {
-      nightmare
-        .goto('http://10.238.41.200/operatorconsole_portlet/standalone_login.jsp')
-        .type('#usrname', 'administrator')
-        .type('#pswd', 'password')
-        .click('.submit')
-        .wait('.nav--primary')
-        .exists('.nav--primary')
-        .end()
-        .then(result => {
-          assert.ok(result)
-          done()
-        })
-        .catch(done)
-    })
-  })
+  // describe('sign in to login page', () => {
+  //   it('should work without error', done => {
+  //     nightmare
+  //       .goto('http://10.238.41.200/operatorconsole_portlet/standalone_login.jsp')
+  //       .type('#usrname', 'administrator')
+  //       .type('#pswd', 'password')
+  //       .click('.submit')
+  //       .wait('#root')
+  //       .end()
+  //       .then(result => { done() })
+  //       .catch(done)
+  //   })
+  // })
+  // describe('intial page load', () => {
+  //   it('should have a title bar logo', done => {
+  //     nightmare
+  //       .goto('http://10.238.41.200/operatorconsole_portlet/standalone_login.jsp')
+  //       .type('#usrname', 'administrator')
+  //       .type('#pswd', 'password')
+  //       .click('.submit')
+  //       .wait('.titlebarheader__logo')
+  //       .exists('.titlebarheader__logo')
+  //       .end()
+  //       .then(result => {
+  //         assert.ok(result)
+  //         done()
+  //       })
+  //       .catch(done)
+  //   })
+  //   it('should have a primary nav', done => {
+  //     nightmare
+  //       .goto('http://10.238.41.200/operatorconsole_portlet/standalone_login.jsp')
+  //       .type('#usrname', 'administrator')
+  //       .type('#pswd', 'password')
+  //       .click('.submit')
+  //       .wait('.nav--primary')
+  //       .exists('.nav--primary')
+  //       .end()
+  //       .then(result => {
+  //         assert.ok(result)
+  //         done()
+  //       })
+  //       .catch(done)
+  //   })
+  // })
 })
